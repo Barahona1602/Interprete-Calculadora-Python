4f90ff578931eab092846faa48c81171f67bed57
Pablo Barahona <pbarahona07@gmail.com>
2022-09-21 22:50:54 -0600

    Actualizacion

--- analizador.py
+++ analizador.py
@@ -2,6 +2,7 @@ from enum import Enum
 from lib2to3.pgen2 import token
 import re
 import math
+from sqlite3 import DateFromTicks
 
 class L_tokens(Enum):
     TK_MENOR = "<"
@@ -23,6 +24,18 @@ class L_tokens(Enum):
     TK_OP_TANGENTE="TANGENTE"
     TK_OP_MOD="MOD"
     TK_E_TIPO = "Tipo"
+    TK_E_TEXTO = "Texto"
+    TK_TEXTO = "[a-zA-Z,À-ÿ\u00f1\u00d1,'+'-'-',''.'*',0.0-9.0,':','%','=','/','^','√','(',')','âˆš']*"
+    TK_E_FUNCION = "Funcion"
+    TK_ESCRIBIR = "ESCRIBIR"
+    TK_E_TITULO = "Titulo"
+    TK_TITULO = "[a-zA-Z,À-ÿ\u00f1\u00d1,'+'-'-',''.'*',0.0-9.0,':','%','=','/','^','√','(',')','âˆš']*"
+    TK_E_COLOR = "Color"
+    TK_COLOR = "['AZUL','VERDE','ROJO','AMARILLO','MORADO','ANARANJADO','NARANJA']"
+    TK_E_TAMANIO = "Tamanio"
+    TK_TAMANIO ="[0-9]*[.]?[0-9]+"
+    TK_E_DESCRIPCION="Descripcion"
+    TK_E_CONTENIDO = "Contenido"
 
 
 class Analizador:
@@ -35,7 +48,12 @@ class Analizador:
         self.archivoimp=archivoimp
         self.op=[]
         self.num=0
-        self.result=[]
+        global result
+        result=[]
+        global datos
+        datos=[]
+        global funcion 
+        funcion = []
 
     def quitar(self, _cadena :str, _num : int):
         _tmp = ""
@@ -97,92 +115,134 @@ class Analizador:
 
                     #SUMA
                     if self.op[0]=="SUMA" and self.num==2:
-                        print(int(self.op[1])+int(self.op[2]))
-                        results=(int(self.op[1])+int(self.op[2]))
-                        self.result.append(results)
+                        print(float(self.op[1])+float(self.op[2]))
+                        results=(float(self.op[1])+float(self.op[2]))
+                        num1=(self.op[1])
+                        num2=(self.op[2])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + num1 + " + " + num2 + " = " + str(results))
+                        result.append(locacion)
 
                     #RESTA
                     if self.op[0]=="RESTA" and self.num==2:
-                        print(int(self.op[1])-int(self.op[2]))
-                        results=(int(self.op[1])+int(self.op[2]))
-                        self.result.append(results)
+                        print(float(self.op[1])-float(self.op[2]))
+                        results=(float(self.op[1])-float(self.op[2]))
+                        num1=(self.op[1])
+                        num2=(self.op[2])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + num1 + " - " + num2 + " = " + str(results))
+                        result.append(locacion)
 
 
 
                     #MULTIPLICACION
                     if self.op[0]=="MULTIPLICACION" and self.num==2:
-                        print(int(self.op[1])*int(self.op[2]))
-                        results=(int(self.op[1])*int(self.op[2]))
-                        self.result.append(results)
+                        print(float(self.op[1])*float(self.op[2]))
+                        results=(float(self.op[1])*float(self.op[2]))
+                        num1=(self.op[1])
+                        num2=(self.op[2])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + num1 + " * " + num2 + " = " + str(results))
+                        result.append(locacion)
+
 
 
 
 
                     #DIVISION
                     if self.op[0]=="DIVISION" and self.num==2:
-                        print(int(self.op[1])/(int(self.op[2])))
-                        results=(int(self.op[1])/(int(self.op[2])))
-                        self.result.append(results)
+                        print(float(self.op[1])/(float(self.op[2])))
+                        results=(float(self.op[1])/(float(self.op[2])))
+                        num1=(self.op[1])
+                        num2=(self.op[2])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + num1 + " / " + num2 + " = " + str(results))
+                        result.append(locacion)
+
 
 
                     #POTENCIA
                     if self.op[0]=="POTENCIA" and self.num==2:
-                        print((int(self.op[2]))**(int(self.op[1])))
-                        results=((int(self.op[2]))**(int(self.op[1])))
-                        self.result.append(results)
+                        print((float(self.op[2]))**(float(self.op[1])))
+                        results=((float(self.op[2]))**(float(self.op[1])))
+                        num1=(self.op[1])
+                        num2=(self.op[2])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + num2 + " ^ " + num1 + " = " + str(results))
+                        result.append(locacion)
 
 
                     
                     #RAIZ
                     if self.op[0]=="RAIZ" and self.num==2:
-                        print((int(self.op[2]))**(1/(int(self.op[1]))))
-                        results=((int(self.op[2]))**(1/(int(self.op[1]))))
-                        self.result.append(results)
+                        print((float(self.op[2]))**(1/(float(self.op[1]))))
+                        results=((float(self.op[2]))**(1/(float(self.op[1]))))
+                        num1=(self.op[1])
+                        num2=(self.op[2])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + num1 + " √ " + num2 + " = " + str(results))
+                        result.append(locacion)
 
 
 
                     #INVERSO
                     if self.op[0]=="INVERSO" and self.num==1:
-                        print(1/(int(self.op[1])))
-                        results=(1/(int(self.op[1])))
-                        self.result.append(results)
+                        print(1/(float(self.op[1])))
+                        results=(1/(float(self.op[1])))
+                        num1=(self.op[1])
+                        operacion=self.op[0]
+                        locacion=(operacion +": "+"1/" + num1 + " = " + str(results))
+                        result.append(locacion)
 
 
                     
                     #SENO
                     if self.op[0]=="SENO" and self.num==1:
-                        print(math.sin(int(self.op[1])))
-                        results=(math.sin(int(self.op[1])))
-                        self.result.append(results)
+                        print(math.sin(float(self.op[1])))
+                        results=(math.sin(float(self.op[1])))
+                        num1=(self.op[1])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + " SIN(" + num1 + ")"+" = " + str(results))
+                        result.append(locacion)
 
 
 
                     #COSENO
                     if self.op[0]=="COSENO" and self.num==1:
-                        print(math.cos(int(self.op[1])))
-                        results=(math.cos(int(self.op[1])))
-                        self.result.append(results)
+                        print(math.cos(float(self.op[1])))
+                        results=(math.cos(float(self.op[1])))
+                        num1=(self.op[1])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + " COS(" + num1 + ")"+" = " + str(results))
+                        result.append(locacion)
 
 
 
                     #TANGENTE
                     if self.op[0]=="TANGENTE" and self.num==1:
-                        print(math.tan(int(self.op[1])))
-                        results=(math.tan(int(self.op[1])))
-                        self.result.append(results)
+                        print(math.tan(float(self.op[1])))
+                        results=(math.tan(float(self.op[1])))
+                        num1=(self.op[1])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + " TAN(" + num1 + ")"+" = " + str(results))
+                        result.append(locacion)
 
 
 
                     #MOD
                     if self.op[0]=="MOD" and self.num==2:
-                        print(int(self.op[1])%int(self.op[2]))
-                        results=(int(self.op[1])%int(self.op[2]))
-                        self.result.append(results)
+                        print(float(self.op[1])%float(self.op[2]))
+                        results=(float(self.op[1])%float(self.op[2]))
+                        num1=(self.op[1])
+                        num2=(self.op[2])
+                        operacion=self.op[0]
+                        locacion=(operacion +": " + num1 + " % " + num2 + " = " + str(results))
+                        result.append(locacion)
 
 
                     #GRANDES
                     if self.num==3 and self.op[0]=="SUMA" and self.op[2]=="SUMA":
-                        print(int(self.op[1])+int(self.op[3])+int(self.op[4]))
+                        print(float(self.op[1])+float(self.op[3])+float(self.op[4]))
                 _cadena = self.quitar(_cadena, s.end())
                 self.aumentarLinea()
             except:
@@ -346,19 +406,116 @@ class Analizador:
         # NUMERO1 OPERADOR NUMERO2
         return {'resultado':_numero, "cadena":_cadena, "Error":False}
 
+
+    def Texto(self, _cadena : str):
+        tokens = [
+            L_tokens.TK_MENOR.value,    # <
+            L_tokens.TK_E_TEXTO.value, # Texto
+            L_tokens.TK_MAYOR.value,    # >
+            L_tokens.TK_TEXTO.value,         # Hola como estas
+            L_tokens.TK_MENOR.value,    # <
+            L_tokens.TK_BARRAINV.value, # /
+            L_tokens.TK_E_TEXTO.value, # Texto
+            L_tokens.TK_MAYOR.value     # >
+        ]
+        _numero = ""
+
+        for i in tokens:
+            try:
+                patron = re.compile(f'^{i}')
+                s = patron.search(_cadena)
+                print("| ", self.linea, " | ", self.columna, " | ", s.group())
+                if i == L_tokens.TK_TEXTO.value:
+                    datos.append(s.group())
+                self.columna += int(s.end())
+                # GUARDAR EL TOKEN
+                _cadena = self.quitar(_cadena, s.end())
+                self.aumentarLinea()
+            except:
+                # GUARDAR ERROR
+                print("Ocurrio un error")
+                return {'resultado':_numero, "cadena":_cadena, "Error": True}
+        self.Funcion(_cadena)
+    
+    def Funcion(self, _cadena : str):
+        tokens = [
+            L_tokens.TK_MENOR.value,    # <
+            L_tokens.TK_E_FUNCION.value, # Funcion
+            L_tokens.TK_IGUAL.value, # =
+            L_tokens.TK_ESCRIBIR.value, #Funcion
+            L_tokens.TK_MAYOR.value,    # >
+            L_tokens.TK_MENOR.value,    # <
+            L_tokens.TK_E_TITULO.value,  #Titulo
+            L_tokens.TK_MAYOR.value,    # >
+            "OPERACIONES",
+            L_tokens.TK_MENOR.value,    # <
+            L_tokens.TK_BARRAINV.value, # /
+            L_tokens.TK_E_TITULO.value, # Titulo
+            L_tokens.TK_MAYOR.value     # >
+        ]
+        _numero = ""
+
+        for i in tokens:
+            try:
+                patron = re.compile(f'^{i}')
+                s = patron.search(_cadena)
+                print("| ", self.linea, " | ", self.columna, " | ", s.group())
+                if i == "OPERACIONES":
+                    funcion.append(s.group())
+                self.columna += int(s.end())
+                # GUARDAR EL TOKEN
+                _cadena = self.quitar(_cadena, s.end())
+                self.aumentarLinea()
+            except:
+                # GUARDAR ERROR
+                print("Ocurrio un error")
+                return {'resultado':_numero, "cadena":_cadena, "Error": True}
+        self.Estilo(_cadena)
+
+    def Estilo(self, _cadena : str):
+        tokens = [
+            L_tokens.TK_MENOR.value,    # <
+            L_tokens.TK_E_FUNCION.value, # Texto
+            L_tokens.TK_IGUAL.value, # =
+            L_tokens.TK_ESCRIBIR.value, #Funcion
+            L_tokens.TK_MAYOR.value,    # >
+            L_tokens.TK_TEXTO.value,         # Hola como estas
+            L_tokens.TK_MENOR.value,    # <
+            L_tokens.TK_BARRAINV.value, # /
+            L_tokens.TK_E_TEXTO.value, # Texto
+            L_tokens.TK_MAYOR.value     # >
+        ]
+        _numero = ""
+
+        for i in tokens:
+            try:
+                patron = re.compile(f'^{i}')
+                s = patron.search(_cadena)
+                print("| ", self.linea, " | ", self.columna, " | ", s.group())
+                if i == L_tokens.TK_TEXTO.value:
+                    datos.append(s.group())
+                self.columna += int(s.end())
+                # GUARDAR EL TOKEN
+                _cadena = self.quitar(_cadena, s.end())
+                self.aumentarLinea()
+            except:
+                # GUARDAR ERROR
+                print("Ocurrio un error")
+                return {'resultado':_numero, "cadena":_cadena, "Error": True}
+
     def Tipo(self, _cadena : str):
         tokens = [
             L_tokens.TK_MENOR.value,        # <
             L_tokens.TK_E_TIPO.value,       # Tipo
             L_tokens.TK_MAYOR.value,        # >
-            "OPERACIONES",                  # OPERACIONES
+            L_tokens.TK_TITULO.value,                  # OPERACIONES
             L_tokens.TK_MENOR.value,        # <
             L_tokens.TK_BARRAINV.value,     # /
             L_tokens.TK_E_TIPO.value,       # Tipo
             L_tokens.TK_MAYOR.value,        # >
         ]
         _numero = ""
-
+    
         for i in tokens:
             try:
                 
@@ -389,12 +546,14 @@ class Analizador:
                 # GUARDAR ERROR
                 print("Ocurrio un error")
                 return {'resultado':_numero, "cadena":_cadena, "Error": True}
+        
+        self.Texto(_cadena)
 
         return {'resultado':_numero, "cadena":_cadena, "Error": False}
 
     def compile(self):
         # LEEMOS EL ARCHIVO DE ENTRADA
-        archivo = open(self.archivoimp, "r")
+        archivo = open(self.archivoimp, "r", encoding="utf-8")
         contenido = archivo.readlines()
         archivo.close()
 
@@ -403,6 +562,8 @@ class Analizador:
         # LIMPIAR MI ENTRADA
         nueva_cadena = ""
         lista_cadena = []
+        global nueva
+        
 
         
 
@@ -412,6 +573,21 @@ class Analizador:
             if i != '':
                 nueva_cadena += i
                 lista_cadena.append(i)
+       
+        for j in contenido:
+            if j!="":
+                lista_cadena.append(j)
+
+        r=0
+
+        for x in range(len(lista_cadena)):
+
+            if lista_cadena[x]=="<Texto>\n":
+                nueva=lista_cadena[x+1]
+                r=2
+                while lista_cadena[x+r]!="</Texto>\n":
+                    nueva+="<p style='color:blue;'>"+lista_cadena[x+r]
+                    r+=1
 
         print(nueva_cadena)
         print(lista_cadena)
@@ -419,3 +595,31 @@ class Analizador:
         self.lista_cadena = lista_cadena
 
         print(self.Tipo(nueva_cadena))
+    
+    def htmlanalizar():
+        r = open("Resultados_202109715.html","w+",encoding="utf-8")
+        cadena="<!DOCTYPE html>\n"
+        cadena+= "<html lang=\"es\">\n"
+        cadena+= "  <head>\n"
+        cadena+="       <meta charset=\"UTF-8\">\n"
+        cadena+="       <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n"
+        cadena+="       <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+        cadena+="</head>\n"
+        cadena += "    <body>\n"
+        cadena += '       <p style="color:blue;">'+nueva+"</p>\n"
+        for i in result:
+            cadena +="        <h1>"+str(i)+"</h1>\n"
+        cadena +="    <body>\n"
+        cadena +="</html>\n"
+        r.writelines(cadena)
+        
+ 
+        
+
+
+    def imprimirlista():
+        for i in (result):
+            print (i)
+
+
+
